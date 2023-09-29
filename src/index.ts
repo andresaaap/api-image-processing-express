1e91123b6a880bc08dd71ac15cd6b42f3b20a145
andresaaap <andresaaap@hotmail.com>
2023-09-29 17:12:45 -0500

    chore: use linter/Prettier for well-formatted code

--- src/index.ts
+++ src/index.ts
@@ -11,59 +11,58 @@ const app = express();
 // and the given filename from the images folder
 
 app.get('/api/images', (req: Request, res: Response) => {
+  //if width, height or filename is not given, send an error message
+  if (!req.query.width || !req.query.height || !req.query.filename) {
+    // send an error message with http status code 400
+    return res.status(400).send('Please provide width, height and filename');
+  }
+  const width: number = Number(req.query.width);
+  const height: number = Number(req.query.height);
+  // else if width or height is not a number, send an error message
+  if (isNaN(width) || isNaN(height)) {
+    // send an error message with http status code 400
+    return res.status(400).send('Please provide width and height as a number');
+  }
 
-        //if width, height or filename is not given, send an error message
-        if (!req.query.width || !req.query.height || !req.query.filename) {
-            // send an error message with http status code 400
-            return res.status(400).send('Please provide width, height and filename');
-        }
-        var width: number = Number(req.query.width);
-        var height: number = Number(req.query.height);
-        // else if width or height is not a number, send an error message
-        if (isNaN(width) || isNaN(height)) {
-            // send an error message with http status code 400
-            return res.status(400).send('Please provide width and height as a number');
-        }
-        
-        // else if width or height is not equal or greater than 10, send an error message
-        if (width < 10 || height < 10) {
-            // send an error message with http status code 400
-            return res.status(400).send('Please provide width and height greater than 10');
-        }
+  // else if width or height is not equal or greater than 10, send an error message
+  if (width < 10 || height < 10) {
+    // send an error message with http status code 400
+    return res
+      .status(400)
+      .send('Please provide width and height greater than 10');
+  }
 
-        // if filename is not a string, send an error message
-        if (typeof req.query.filename !== 'string') {
-            // send an error message with http status code 400
-            return res.status(400).send('Please provide filename as a string');
-        }
-        // else if filename is an empty string, send an error message
-        else if (req.query.filename === '') {
-            // send an error message with http status code 400
-            return res.status(400).send('Please provide filename');
-        }
+  // if filename is not a string, send an error message
+  if (typeof req.query.filename !== 'string') {
+    // send an error message with http status code 400
+    return res.status(400).send('Please provide filename as a string');
+  }
+  // else if filename is an empty string, send an error message
+  else if (req.query.filename === '') {
+    // send an error message with http status code 400
+    return res.status(400).send('Please provide filename');
+  }
 
-        // convert req.query.filename to string
-        var filename: string = <string>(<unknown>req.query.filename);
+  // convert req.query.filename to string
+  const filename: string = <string>(<unknown>req.query.filename);
 
-        var image = new Image(width, height, filename);
+  const image = new Image(width, height, filename);
 
-        // resize the image with transformerService
-        var transformerService = new TransformerService();
-        transformerService.resize(image.filename, image.width, image.height)
-        .then((resizedImageBuffer) => {
-            // in the response set the content-type to image/jpeg
-            res.setHeader('Content-Type', 'image/jpeg');
-            // send the resized image
-            return res.send(resizedImageBuffer);
-        })
-        .catch((err) => {
-            console.log(err);
-            // if there is an error, send the error message
-            return res.status(500).send(err);
-        });
-
-        
-    }
-);
+  // resize the image with transformerService
+  const transformerService = new TransformerService();
+  transformerService
+    .resize(image.filename, image.width, image.height)
+    .then((resizedImageBuffer) => {
+      // in the response set the content-type to image/jpeg
+      res.setHeader('Content-Type', 'image/jpeg');
+      // send the resized image
+      return res.send(resizedImageBuffer);
+    })
+    .catch((err) => {
+      console.log(err);
+      // if there is an error, send the error message
+      return res.status(500).send(err);
+    });
+});
 
 app.listen(3000, () => console.log('Server running'));
--- src/services/TransformerService.ts
+++ src/services/TransformerService.ts
@@ -8,69 +8,73 @@ import path from 'path';
 // that resolves to a Buffer with the resized image
 
 class TransformerService {
-    public resize(filename: string, width: number, height: number): Promise<Buffer> {
-         // return a promise that resolves to a Buffer with the resized image
-        return new Promise<Buffer>((resolve, reject) => {
-            // create the resized image filename using the original filename, width and height
-            var filenameParts = filename.split('.');
-            var resizedFilename = `${filenameParts[0]}_${width}_${height}.${filenameParts[1]}`;
-            var thumbnailsPath = path.join(__dirname, '../thumbnails');
-            // if the resized image already exists in the thumbnails folder
-            fs.stat(`${thumbnailsPath}/${resizedFilename}`)
-            .then((stats) => {
-                // if the resized image already exists in the thumbnails folder
-                if (stats.isFile()) {
-                    // read the resized image from the thumbnails folder
-                    fs.readFile(`${thumbnailsPath}/${resizedFilename}`)
-                    .then((data) => {
-                        // resolve the promise with the resized image
-                        resolve(data);
-                    });
-                }
+  public resize(
+    filename: string,
+    width: number,
+    height: number
+  ): Promise<Buffer> {
+    // return a promise that resolves to a Buffer with the resized image
+    return new Promise<Buffer>((resolve, reject) => {
+      // create the resized image filename using the original filename, width and height
+      const filenameParts = filename.split('.');
+      const resizedFilename = `${filenameParts[0]}_${width}_${height}.${filenameParts[1]}`;
+      const thumbnailsPath = path.join(__dirname, '../thumbnails');
+      // if the resized image already exists in the thumbnails folder
+      fs.stat(`${thumbnailsPath}/${resizedFilename}`)
+        .then((stats) => {
+          // if the resized image already exists in the thumbnails folder
+          if (stats.isFile()) {
+            // read the resized image from the thumbnails folder
+            fs.readFile(`${thumbnailsPath}/${resizedFilename}`).then((data) => {
+              // resolve the promise with the resized image
+              resolve(data);
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
 
+          const imgPath = path.join(__dirname, '../images', filename);
+
+          let resizedImageBuffer: Buffer;
+
+          // read the image from the images folder
+          fs.readFile(imgPath)
+            .then((data) => {
+              // resize the image
+              sharp(data)
+                .resize(width, height)
+                .toBuffer()
+                .then((result: Buffer) => {
+                  resizedImageBuffer = result;
+
+                  // create the thumbnails folder if it doesn't exist
+                  return fs.mkdir(thumbnailsPath, { recursive: true });
+                })
+                .then(() => {
+                  // save the resized image to the thumbnails folder
+                  return fs.writeFile(
+                    `${thumbnailsPath}/${resizedFilename}`,
+                    resizedImageBuffer
+                  );
+                })
+                .then(() => {
+                  // resolve the promise with the resized image
+                  resolve(resizedImageBuffer);
+                })
+                .catch((err) => {
+                  // reject the promise with the error
+                  reject(err);
+                });
             })
             .catch((err) => {
-                    console.log(err);
+              // reject the promise with the error
 
-                    var imgPath = path.join(__dirname, '../images', filename);
-
-                    var resizedImageBuffer: Buffer;
-
-                    // read the image from the images folder
-                    fs.readFile(imgPath)
-                    .then((data) => {
-                        // resize the image
-                        sharp(data)
-                        .resize(width, height)
-                        .toBuffer()
-                        .then((result: Buffer) => {
-                            resizedImageBuffer = result;
-                            
-                            // create the thumbnails folder if it doesn't exist
-                            return fs.mkdir(thumbnailsPath, { recursive: true });
-                        })
-                        .then(() => {
-                            // save the resized image to the thumbnails folder
-                            return fs.writeFile(`${thumbnailsPath}/${resizedFilename}`, resizedImageBuffer);
-                        })
-                        .then(() => {
-                            // resolve the promise with the resized image
-                            resolve(resizedImageBuffer);
-                        })
-                        .catch((err) => {
-                            // reject the promise with the error
-                            reject(err);
-                        });
-                    })
-                    .catch((err) => {
-                        // reject the promise with the error
-                        
-                        reject(err);
-                    });
-                }
-            );
+              reject(err);
+            });
         });
-    }
+    });
+  }
 }
 
 export default TransformerService;
